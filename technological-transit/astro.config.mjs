e17e5c634564c6f82ebadf3a1995726c9cc70d71
Jan Alvin Dimla <alvindimla@yahoo.com>
2026-03-20 22:32:29 +0800

    initial commit

---
+++ technological-transit/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+    base: '/',
+    //trailingSlash: 'always',
+    output: 'static', 
+
+    vite: {
+        ssr: {
+        external: []
+        }
+    },
+
+});
